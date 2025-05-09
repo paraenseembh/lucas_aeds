3eb45e66c2634d9688f63c23a9c982bc7bd081c1
Lucas Rafael P. do Nascimento <113736193+paraenseembh@users.noreply.github.com>
2025-05-09 12:31:01 -0300

    ADD prova2

---
+++ questao2.java
@@ -0,0 +1,27 @@
+/*Entrada
+A entrada é composta de vários casos de teste. 
+A primeira linha de cada caso de teste contém dois números inteiros N e P,
+ indicando, respectivamente, o número de caixas e o número de pilhas (1 ≤ P ≤ N ≤ 1.000).
+  As caixas são numeradas sequencialmente de 1 a N.
+
+Cada uma das P linhas seguintes descreve uma pilha. Cada linha contém um inteiro Qi,
+ indicando quantas caixas há na pilha i, seguido de um espaço em branco, seguido de uma lista de Qi
+ números, que são os identificadores das caixas. Os elementos da lista são separados por um espaço
+ em branco. Todas as pilhas contêm pelo menos uma caixa, 
+ e todas as caixas aparecem exatamente uma vez na entrada. 
+ As caixas em cada pilha são listadas em ordem, da base até o topo da pilha. Todas as caixas têm o mesmo formato. O final da entrada é indicado por N = P = 0. 
+ A entrada deve ser lida da entrada padrão.
+
+
+
+
+Saída
+Para cada caso de teste da entrada, seu programa deve imprimir uma única linha, contendo um único inteiro: o número mínimo de caixas, além da caixa 1, que Joãozinho precisa desempilhar para recuperar o seu inventário. A saída deve ser escrita na saída padrão. */
+
+public class questao2 {
+    
+    public static void main(String[] args) {
+    int Q = 0, P = 0; 
+    //CONDICAO DE ṔARADA
+    }
+}
